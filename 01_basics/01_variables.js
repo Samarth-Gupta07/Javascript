d8d8710aad1d0b225447e4579ae17ad3ea6b0aff
[Samarth Gupta] <[guptasamarth343@gmail.com]>
2026-07-01 20:55:54 +0530

    Learn Javascript Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const ID= 12345
+let email = "abc@example.com"
+var password = "password123"
+city = "New York" 
+let state ;
+
+email = "def@example.com"; 
+password = "newpassword123"; 
+city= "delhi"
+
+console.table([ID, email, password, city, state]);
