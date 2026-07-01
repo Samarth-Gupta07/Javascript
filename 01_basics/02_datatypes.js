b09ea16b1e072a00ba88f94f4b4862a3144641c4
[Samarth Gupta] <[guptasamarth343@gmail.com]>
2026-07-01 22:59:29 +0530

    Learn Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+/* different data types in javascript */
+
+// numbers 
+// bigint
+// string
+// boolean
+// null  (standalone value)
+// undefined
+// symbol
+// object
+
+console.log(typeof 42); // number
+console.log(typeof 9007199254740991n); // bigint
+console.log(typeof "Hello, World!"); // string
+console.log(typeof true);   // boolean
+console.log(typeof null); // object (this is a known quirk in JavaScript)
+console.log(typeof undefined); // undefined
